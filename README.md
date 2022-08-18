b71808cb5591d2ecb8d654a2823febe5843767a9
cristian-ratoi <cristian.ratoi@vitals.co>
2022-08-18 17:52:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ldxkzy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ldxkzy)
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { MessagePageComponent } from './pages/message-page/message-page.component';
+
+const routes: Route[] = [
+  { path: 'message/:id', component: MessagePageComponent },
+];
+
+@NgModule({
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
